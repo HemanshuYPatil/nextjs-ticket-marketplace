3606f91aded6c766050733f167f4e90559f06de5
Eray Demircioglu <c.eray@hotmail.com>
2024-11-26 18:32:46 +0300

    Ticket Marketplace

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "exciting-caterpillar-231.convex.cloud", protocol: "https" },
+    ],
+  },
 };
 
 export default nextConfig;
